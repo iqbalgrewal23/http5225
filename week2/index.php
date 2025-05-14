b1a6d6b53a7f59488553e41e1072448dd3a5bb96
iqbal singh <iqbal.grewal38@gmail.com>
2025-05-14 19:59:26 -0400

    second week lab

---
+++ week2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <?php
+    $fname="iqbal";
+    $lname="singh";
+    echo"<h1>hello hello</h1>";
+    echo"hello". $fname." ".$lname;
+    echo"hello,welcome to iqbal\"s pind";
+    ?>
+</body>
+</html>
